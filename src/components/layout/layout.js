0c446c95954c46e3359c543b13b389f9d8883471
Stephen Karpeles <stephen@stephenkarpeles.com>
2022-08-26 14:26:03 -0500

    Fix all local errors

---
+++ src/components/layout/layout.js
@@ -0,0 +1,21 @@
+import React from "react"
+import "../base.css"
+import "./layout.css"
+import { Helmet } from "react-helmet";
+import Header from "../header/header"
+import Footer from "../footer/footer"
+
+const Layout = props => {
+  return (
+    <div id="App">
+      <Helmet>
+        <title>{props.pageTitle}</title>
+      </Helmet>
+      <Header />      
+      {props.children}
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
