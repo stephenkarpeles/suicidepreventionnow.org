735b242231458f6de362ec5e240b6299051e4dfe
Stephen Karpeles <stephen@stephenkarpeles.com>
2022-09-13 10:09:13 -0500

    Update styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,3 @@
 import "@fontsource/merriweather-sans" // Defaults to weight 400 with all styles included.
 import "@fontsource/merriweather-sans/300.css" // 300 weight
+import "@fontsource/merriweather-sans/800.css" // 900 weight
