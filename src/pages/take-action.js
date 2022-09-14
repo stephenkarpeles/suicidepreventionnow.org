f31f270d1dfb76b97c239d821076bf29ab7a2c09
Shawn Pais <spais@oomphinc.com>
2022-09-14 15:58:44 -0400

    Component: Social Media 3 Columns

--- src/pages/take-action.js
+++ src/pages/take-action.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { Link } from "gatsby";
 
 // Components
-import Layout from "../components/layout/layout"
+import Layout from "../components/layout/layout";
+
+import placeholderImage from '../images/placeholder-image.png';
 
 const TakeAction = () => {
 
@@ -14,6 +17,83 @@ const TakeAction = () => {
           </h2>
           <div className="c-one-col-block__content">
             <p>The data shows that the publics wants to take action to strengthen suicide prevention and mental health. To help spur action and create better systems of care, check out the actionable resources below and share widely.</p>
+          </div>
+        </div>
+      </section>
+      <section className="t-section-lg">
+        <div className="c-social-media-3col container">
+          <h2 className="c-social-media-3col__header-title">
+            <span>Share these Messages</span>
+          </h2>
+          <div className="c-social-media-3col__content">
+            <div className="row">
+              <div className="col-1-3">
+                <div className="c-social-card">
+                  <figure className="c-social-card__figure">
+                    <img src={placeholderImage} alt="" className="c-social-card__image" />
+                  </figure>
+                  <div className="c-social-card__links">
+                    <Link to={'/'} className="o-resource-links o-resource-links--down-arrow-icon"><span>Download this chart</span></Link>
+                    <Link to={'/'} className="o-resource-links o-resource-links--share-icon"><span>Share</span></Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-1-3">
+                <div className="c-social-card">
+                  <figure className="c-social-card__figure">
+                    <img src={placeholderImage} alt="" className="c-social-card__image" />
+                  </figure>
+                  <div className="c-social-card__links">
+                    <Link to={'/'} className="o-resource-links o-resource-links--down-arrow-icon"><span>Download this chart</span></Link>
+                    <Link to={'/'} className="o-resource-links o-resource-links--share-icon"><span>Share</span></Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-1-3">
+                <div className="c-social-card">
+                  <figure className="c-social-card__figure">
+                    <img src={placeholderImage} alt="" className="c-social-card__image" />
+                  </figure>
+                  <div className="c-social-card__links">
+                    <Link to={'/'} className="o-resource-links o-resource-links--down-arrow-icon"><span>Download this chart</span></Link>
+                    <Link to={'/'} className="o-resource-links o-resource-links--share-icon"><span>Share</span></Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-1-3">
+                <div className="c-social-card">
+                  <figure className="c-social-card__figure">
+                    <img src={placeholderImage} alt="" className="c-social-card__image" />
+                  </figure>
+                  <div className="c-social-card__links">
+                    <Link to={'/'} className="o-resource-links o-resource-links--down-arrow-icon"><span>Download this chart</span></Link>
+                    <Link to={'/'} className="o-resource-links o-resource-links--share-icon"><span>Share</span></Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-1-3">
+                <div className="c-social-card">
+                  <figure className="c-social-card__figure">
+                    <img src={placeholderImage} alt="" className="c-social-card__image" />
+                  </figure>
+                  <div className="c-social-card__links">
+                    <Link to={'/'} className="o-resource-links o-resource-links--down-arrow-icon"><span>Download this chart</span></Link>
+                    <Link to={'/'} className="o-resource-links o-resource-links--share-icon"><span>Share</span></Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-1-3">
+                <div className="c-social-card">
+                  <figure className="c-social-card__figure">
+                    <img src={placeholderImage} alt="" className="c-social-card__image" />
+                  </figure>
+                  <div className="c-social-card__links">
+                    <Link to={'/'} className="o-resource-links o-resource-links--down-arrow-icon"><span>Download this chart</span></Link>
+                    <Link to={'/'} className="o-resource-links o-resource-links--share-icon"><span>Share</span></Link>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
