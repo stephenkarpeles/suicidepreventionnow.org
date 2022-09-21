9aecda57a71b9a9cb2bd965fec91a5fb61b63ae5
Stephen Karpeles <stephen@stephenkarpeles.com>
2022-09-21 13:23:47 -0500

    Update smooth scroll

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -23,6 +23,13 @@ export const useScrollHandler = () => {
 const Header = () => {
   const scroll = useScrollHandler();
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
   return (
     <header className={`header${scroll ? " header--user-has-scrolled" : ""}`}>
       <div className="header__base-gradient">
@@ -72,6 +79,9 @@ const Header = () => {
               </ul>
             </div>
           </div>
+        </div>
+        <div className="header__back-to-top" onClick={scrollToTop}>
+          <svg viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="arrow-circle-up"><rect width="24" height="24" opacity="0" /><path className="header__back-to-top-btn" d="M14.31 12.41L13 13.66V8a1 1 0 0 0-2 0v5.59l-1.29-1.3a1 1 0 0 0-1.42 1.42l3 3a1 1 0 0 0 .33.21.94.94 0 0 0 .76 0 .54.54 0 0 0 .16-.1.49.49 0 0 0 .15-.1l3-2.86a1 1 0 0 0-1.38-1.45z"/><path className="header__back-to-top-btn" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8 8 0 0 1-8 8z"/></g></g></svg>
         </div>
       </div>
     </header>
